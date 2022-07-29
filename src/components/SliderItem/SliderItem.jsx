857083ca6068ff19f3cb3e4cf8b1c5ad43383b47
Khanh Duy <tranhakhanhduy2001@gmail.com>
2022-07-29 10:24:15 +0700

    fix arrow icon next

--- src/components/SliderItem/SliderItem.jsx
+++ src/components/SliderItem/SliderItem.jsx
@@ -42,7 +42,12 @@ export default function SliderItem({name, time, date, content, nextName,image}) 
          <span className="sliderItem-next-text">Next show</span>
          <span className="sliderItem-next-name">{nextName}</span>
        </div>
-       
+        <div className="sliderItem-icon">
+        <svg width="19" height="16" viewBox="0 0 19 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M6.35288e-05 9.00003L14.5861 9.00003L9.29306 14.293L10.7071 15.707L18.4141 8.00003L10.7071 0.293028L9.29306 1.70703L14.5861 7.00003L6.37037e-05 7.00003L6.35288e-05 9.00003Z" fill="#B4B4B4"/>
+</svg>
+
+        </div>
       </div>
     </>
   );
