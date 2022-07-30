78cc3ddadbde8224494c1d3aa3bd85f775e6a49f
Khanh Duy <tranhakhanhduy2001@gmail.com>
2022-07-30 12:09:17 +0700

    fix view 100vh

--- src/components/SliderItem/SliderItem.jsx
+++ src/components/SliderItem/SliderItem.jsx
@@ -8,8 +8,6 @@ export default function SliderItem({
   nextName,
   image,
 }) {
-
-
   return (
     <>
       <div className="sliderItem">
@@ -20,9 +18,7 @@ export default function SliderItem({
             <span>{date}</span>
           </div>
           <span className="sliderItem-info-name">{name}</span>
-          <p className="sliderItem-info-content">
-            {content}
-          </p>
+          <p className="sliderItem-info-content">{content}</p>
           <button className="sliderItem-info-button">
             <span>Buy Ticket</span>
             <svg
@@ -48,29 +44,21 @@ export default function SliderItem({
           <span className="sliderItem-next-text">Next show</span>
           <span className="sliderItem-next-name">{nextName}</span>
         </div>
-        <div
-          className="sliderItem-icon nextEl"
-        >
+        <div className="sliderItem-icon nextEl">
           <svg
             width="19"
             height="16"
             viewBox="0 0 19 16"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
+            className="nextEl"
           >
             <path
               d="M6.35288e-05 9.00003L14.5861 9.00003L9.29306 14.293L10.7071 15.707L18.4141 8.00003L10.7071 0.293028L9.29306 1.70703L14.5861 7.00003L6.37037e-05 7.00003L6.35288e-05 9.00003Z"
               fill="#B4B4B4"
+            className="nextEl"
+
             />
-          </svg>
-        </div>
-        <div className="sliderItem-next">
-          <span className="sliderItem-next-text">Next show</span>
-          <span className="sliderItem-next-name">{nextName}</span>
-        </div>
-        <div className="sliderItem-icon">
-          <svg width="19" height="16" viewBox="0 0 19 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M6.35288e-05 9.00003L14.5861 9.00003L9.29306 14.293L10.7071 15.707L18.4141 8.00003L10.7071 0.293028L9.29306 1.70703L14.5861 7.00003L6.37037e-05 7.00003L6.35288e-05 9.00003Z" fill="#B4B4B4" />
           </svg>
         </div>
       </div>
